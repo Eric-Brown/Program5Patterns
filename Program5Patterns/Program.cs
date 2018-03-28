6f8321f9c9f269decf7d30d06d56d51398597307
Eric Brown <alexander7hegreat@gmail.com>
2018-03-28 15:26:40 -0600

    Driver program created.

--- Program5Patterns/Program.cs
+++ Program5Patterns/Program.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Program5Patterns
 {
+    /// <summary>
+    /// Class that is a test driver for the shapes hierarchy
+    /// </summary>
     class Program
     {
+        private static List<Shape> shapes = new List<Shape>();
+        private enum KNOWNSHAPETYPES { LINE, RECTANGLE, POINT, XXCIRCLE}
+        private const uint NUM_SHAPES_TO_CREATE = 40;
+        /// <summary>
+        /// Entry point for test driver.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             /*
@@ -16,6 +27,56 @@ namespace Program5Patterns
              * all of the shapes it contains, verifying that your Circle class nicely 
              * coexists with the other types of Shape.
              */
+            PopulateShapes();
+            DisplayShapes();
+            SetShapes();
+            UndisplayShapes();
+            Console.ReadKey();
+        }
+
+        private static void UndisplayShapes()
+        {
+            foreach (var shape in shapes)
+                shape.undisplay();
+        }
+
+        private static void SetShapes()
+        {
+            foreach (var shape in shapes)
+                shape.setLocation();
+        }
+
+        private static void DisplayShapes()
+        {
+            foreach (var shape in shapes)
+                shape.display();
+        }
+
+        private static void PopulateShapes()
+        {
+            Random random = new Random();
+            var types = Enum.GetValues(typeof(KNOWNSHAPETYPES));
+            for (int i = 0; i < NUM_SHAPES_TO_CREATE; i++)
+            {
+                KNOWNSHAPETYPES choosen = (KNOWNSHAPETYPES)types.GetValue(random.Next(types.Length));
+                switch(choosen)
+                {
+                    case KNOWNSHAPETYPES.LINE:
+                        shapes.Add(new Line());
+                        break;
+                    case KNOWNSHAPETYPES.POINT:
+                        shapes.Add(new Point());
+                        break;
+                    case KNOWNSHAPETYPES.RECTANGLE:
+                        shapes.Add(new Rectangle());
+                        break;
+                    case KNOWNSHAPETYPES.XXCIRCLE:
+                        shapes.Add(new XXCircleAdapter(new XXCircle()));
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
